68136b2e502548654058ee792f075bbf1da642eb
lsivilotti <luca.sivilotti@gmail.com>
2025-02-15 17:24:05 -0500

    R07 part 2, version 1.0: uncommented lines are to run the first part of step 4, other commented lines are for subsequent steps

--- R07_Activity/main.cpp
+++ R07_Activity/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
  * @brief Code for Activity R07
- * @date 02/14/2025
+ * @date 02/15/2025
  * @author Luca Sivilotti
  */
 
@@ -11,6 +11,12 @@
 #include <FEHUtility.h>
 #include <FEHMotor.h>
 
+/*Direction constants*/
+#define LEFT -1
+#define RIGHT 1
+#define BACKWARDS -1
+#define FORWARDS 1
+
 /*Switch constants*/
 #define ON 0
 #define OFF 1
@@ -19,6 +25,9 @@
 #define VOLTAGE 9.0
 #define F_POWER 12.
 #define B_POWER -12.
+
+/*Encoder constants*/
+#define UNIT_COUNTS 40.489
 
 /*Optosensor constants*/
 #define L_DIV 1.45
@@ -35,9 +44,11 @@ enum Line
     LINE_OFF_RIGHT
 };
 
-/*Optosensor*/
+/*Left Optosensor*/
 AnalogInputPin optol(FEHIO::P1_0);
+/*Middle Optosensor*/
 AnalogInputPin optom(FEHIO::P1_1);
+/*Right Optosensor*/
 AnalogInputPin optor(FEHIO::P1_2);
 /*Front right switch*/
 DigitalInputPin fr(FEHIO::P2_0);
@@ -51,6 +62,10 @@ DigitalInputPin bl(FEHIO::P2_7);
 FEHMotor rightMotor(FEHMotor::Motor0, VOLTAGE);
 /*Motor powering left wheel*/
 FEHMotor leftMotor(FEHMotor::Motor1, VOLTAGE);
+/*Right encoder*/
+DigitalEncoder encoderR(FEHIO::P0_0);
+/*Left encoder*/
+DigitalEncoder encoderL(FEHIO::P0_1);
 
 /*Methods*/
 int followLine(int);
@@ -58,6 +73,9 @@ int stateSense(int);
 void turnOff(int);
 void turnOn(int);
 void straight();
+void forward(int, int);
+void turn(int, int, int);
+void stop();
 
 /**
  * @brief Main method.
@@ -70,11 +88,59 @@ int main(void)
         ;
     while (LCD.Touch(&x, &y))
         ;
-    int prevState = LINE_MIDDLE;
-    while (fr.Value() == OFF && fl.Value() == OFF)
-    {
-        prevState = followLine(prevState);
-    }
+    /*
+    Part 1
+    */
+    // int prevState = LINE_MIDDLE;
+    // while (fr.Value() == OFF && fl.Value() == OFF)
+    // {
+    //     prevState = followLine(prevState);
+    // }
+    // stop();
+
+    /*
+    Part 2: Step 4
+    */
+    int encoderCounts = UNIT_COUNTS * 6;
+    int motorPercent = 25;
+    // int motorPercent = 40;
+    // int motorPercent = 60;
+    forward(motorPercent, encoderCounts);
+
+    /*
+    Part 2: Step 5
+    */
+    // int encoderCounts = UNIT_COUNTS * (2 * 3.141592 * 3) / 4;
+    // int motorPercent = 25;
+    // int motorPercent = 40;
+    // int motorPercent = 60;
+    // turn(motorPercent, encoderCounts, RIGHT);
+
+    /*
+    Part 2: Step 6
+    */
+    // int encoderCounts = UNIT_COUNTS * (2 * 3.141592 * 3) / 4;
+    // int motorPercent = 25;
+    // int motorPercent = 40;
+    // int motorPercent = 60;
+    // turn(motorPercent, encoderCounts, LEFT);
+
+    /*
+    Part 2: Step 7
+    */
+    // int encoderCounts = UNIT_COUNTS * 6;
+    // int encoderCounts = UNIT_COUNTS * (2 * 3.141592 * 3) / 4;
+    // int motorPercent = 25;
+    // int motorPercent = 40;
+    // int motorPercent = 60;
+    // forward(motorPercent, UNIT_COUNTS * 14);
+    // turn(motorPercent, UNIT_COUNTS * (2 * 3.141592 * 3) / 4, LEFT);
+    // forward(motorPercent, UNIT_COUNTS * 10);
+    // turn(motorPercent, UNIT_COUNTS * (2 * 3.141592 * 3) / 4, RIGHT);
+    // forward(motorPercent, UNIT_COUNTS * 4);
+
+    int actualCountsL = encoderL.Counts();
+    int actualCountsR = encoderR.Counts();
 }
 
 /**
@@ -88,16 +154,16 @@ int followLine(int prevState)
     switch (state)
     {
     case LINE_OFF_LEFT:
-        turnOff(-1);
+        turnOff(LEFT);
         break;
     case LINE_OFF_RIGHT:
-        turnOff(1);
+        turnOff(RIGHT);
         break;
     case LINE_ON_LEFT:
-        turnOff(-1);
+        turnOff(LEFT);
         break;
     case LINE_ON_RIGHT:
-        turnOff(1);
+        turnOff(RIGHT);
         break;
     default:
         straight();
@@ -193,4 +259,53 @@ void straight()
 {
     rightMotor.SetPercent(F_POWER);
     rightMotor.SetPercent(F_POWER);
+}
+
+/**
+ * @brief Moves robot forwards a specified amount.
+ * @param percent motor speed
+ * @param counts counts of the encoder on the wheel
+ */
+void forward(int percent, int counts)
+{
+    encoderR.ResetCounts();
+    encoderL.ResetCounts();
+
+    rightMotor.SetPercent(percent);
+    leftMotor.SetPercent(percent);
+
+    while (encoderR.Counts() < counts && encoderL.Counts() < counts)
+        ;
+
+    stop();
+}
+
+/**
+ * @brief Turns the robot in place a specified amount.
+ * @param percent motor speed
+ * @param counts counts of the encoder on the wheel
+ * @param dir direction the robot turns (-1 for left, 1 for right)
+ */
+void turn(int percent, int counts, int dir)
+{
+    encoderR.ResetCounts();
+    encoderL.ResetCounts();
+
+    rightMotor.SetPercent(percent * -1 * dir);
+    leftMotor.SetPercent(percent * dir);
+
+    while (encoderR.Counts() < counts && encoderL.Counts() < counts)
+        ;
+
+    stop();
+}
+
+/**
+ * @brief Stops the robot.
+ */
+void stop()
+{
+    rightMotor.Stop();
+    leftMotor.Stop();
+    Sleep(0.5);
 }
