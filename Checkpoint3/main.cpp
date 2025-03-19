6ff81c0efa7f395dbf48137e7f7b63b1f4307109
lsivilotti <luca.sivilotti@gmail.com>
2025-03-19 13:14:39 -0400

    Robot Milestone 3, code finished and untested

--- Checkpoint3/main.cpp
+++ Checkpoint3/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
- * @brief Code for LALA
- * @date 03/07/2025
+ * @brief Code for Checkpoint 3
+ * @date 03/19/2025
  * @author LALA
  */
 
@@ -20,8 +20,6 @@
 #define IDENTIFIER "1130D6KKR"
 /*Width of robot in inches*/
 #define BOT_WIDTH 7.25
-/*Value of pi*/
-#define PI 3.141592
 /*Degrees in a circle*/
 #define DEGREES 360.
 
@@ -126,52 +124,55 @@ DigitalEncoder encoderL(FEHIO::P0_7);
 /*Methods–––––––––––––––––––––––––––––––––––––––––––*/
 
 void activateHumidifier(Color);
+void drive(float, double);
 float findCDS();
-Color getCDS(float);
-float motorSpeed(float);
 void findLine();
 Line followLine(Line);
 Line followLine(Line, float);
+Color getCDS(float);
+float motorSpeed(float);
+void moveWindow(Direction);
+void openCloseWindow();
+void rotate(float, float, Direction);
 Line stateSense(Line);
+void stop();
+void straight(float);
+void turn(float, float, Direction);
 void turnOff(float, Direction);
 void turnOn(float, Direction);
-void straight(float);
-void forward(float, double);
-void rotate(float, float, Direction);
-void turn(float, float, Direction);
-void stop();
+void windowReposition();
 
 /**
  * @brief Main method.
  */
 int main(void)
 {
+    float buttonPressSpeed = 2 * F_POWER;
     LCD.Clear();
     while (cds.Value() > NONE_LIM)
     {
         LCD.Write(cds.Value());
         Sleep(0.25);
     }
-    forward(50, 2);
-    forward(B_POWER, 1);
+    drive(buttonPressSpeed, 2.);
+    drive(B_POWER, 1.);
     rotate(F_POWER, 135, LEFT);
-    forward(F_POWER, 40.);
+    drive(F_POWER, 35.);
     rotate(F_POWER, 90., LEFT);
-    forward(B_POWER, 4.5);
-    Color color = Color::NONE;
-    activateHumidifier(color);
+    drive(B_POWER, 5.);
+    drive(F_POWER, 18.);
+    openCloseWindow();
 }
 
 /**
  * @brief Navigates to and presses humidifier button.
  *
  * @param col color of the humidifer light
+ *
+ * @todo change instructions to use optosensors
  */
 void activateHumidifier(Color col)
 {
-    /**
-     * @todo change instructions to use optosensors
-     */
     while (col == NONE)
     {
         /*looks for then gets the color of LED*/
@@ -182,21 +183,21 @@ void activateHumidifier(Color col)
             /*if red*/
         case FIRE:
             rotate(F_POWER, 90, RIGHT);
-            forward(F_POWER, 1);
+            drive(F_POWER, 1);
             rotate(F_POWER, 90, LEFT);
-            forward(F_POWER * 1.5, 7);
+            drive(F_POWER * 1.5, 7);
             break;
             /*if blue*/
         case WATER:
             rotate(F_POWER, 90, LEFT);
-            forward(F_POWER, 1);
+            drive(F_POWER, 1);
             rotate(F_POWER, 90, RIGHT);
-            forward(F_POWER * 1.5, 7);
+            drive(F_POWER * 1.5, 7);
             break;
             /*if value is outside red or blue*/
         case NONE:
             LCD.WriteLine("404");
-            forward(B_POWER, 21);
+            drive(B_POWER, 21);
             break;
         default:
             break;
@@ -205,16 +206,50 @@ void activateHumidifier(Color col)
 }
 
 /**
+ * @brief Moves robot a specified amount.
+ *
+ * @param percent motor speed (if < 0 robot will drive backwards)
+ * @param dist distance for the bot to travel (inches)
+ */
+void drive(float percent, double dist)
+{
+    /*
+    Translates distance to counts
+
+    Explanation of multiplication by 2 is in while loop
+    */
+    int counts = 2 * (UNIT_COUNTS * dist);
+
+    encoderR.ResetCounts();
+    encoderL.ResetCounts();
+
+    /*Actual power directed to motors to produce desired speed*/
+    float speed = motorSpeed(percent);
+
+    straight(speed);
+
+    /*
+    Takes the average of the encoder counts and compares to (desired) counts
+
+    Division by 2 for average moved to other side of inequality and into
+    calculation of counts to save fractions of a second in computation time each loop
+    */
+    while (encoderR.Counts() + encoderL.Counts() < counts)
+        ;
+
+    stop();
+}
+
+/**
  * @brief Positions robot over light source.
  *
  * @return value read by CdS
+ *
+ * @todo Change to use optosensors
  */
 float findCDS()
 {
-    /**
-     * @todo Change to use optosensors
-     */
-    forward(F_POWER, 21.);
+    drive(F_POWER, 21.);
     float minCDS = 3.3;
     float val = cds.Value();
     int count = 0;
@@ -228,11 +263,11 @@ float findCDS()
         }
         if (round < 4)
         {
-            forward(B_POWER, 0.5);
+            drive(B_POWER, 0.5);
         }
         else if (round > 4 && round < 10)
         {
-            forward(F_POWER, 0.5);
+            drive(F_POWER, 0.5);
         }
         else
         {
@@ -250,44 +285,6 @@ float findCDS()
     }
     rotate(F_POWER, turned % (int)DEGREES, LEFT);
     return minCDS;
-}
-
-/**
- * @brief Determines the color of the light sensed by CdS.
- *
- * @param val value read by CdS
- * @return a value in Color {NONE, WATER, FIRE}
- */
-Color getCDS(float val)
-{
-    /*Compares val to Color limits, returning state of color*/
-    if (val > NONE_LIM)
-    {
-        return NONE;
-    }
-    else if (val > BLUE_LIM)
-    {
-        return WATER;
-    }
-    else if (val > RED_LIM)
-    {
-        return FIRE;
-    }
-    else
-    {
-        return NONE;
-    }
-}
-
-/**
- * @brief Calculates actual motor speed based on remaining battery power.
- *
- * @param percent desired motor percent
- * @return actual motor percent
- */
-float motorSpeed(float percent)
-{
-    return (11.5 / Battery.Voltage()) * percent;
 }
 
 /**
@@ -386,6 +383,132 @@ Line followLine(Line prevState, float dist)
 }
 
 /**
+ * @brief Determines the color of the light sensed by CdS.
+ *
+ * @param val value read by CdS
+ * @return a value in Color {NONE, WATER, FIRE}
+ */
+Color getCDS(float val)
+{
+    /*Compares val to Color limits, returning state of color*/
+    if (val > NONE_LIM)
+    {
+        return NONE;
+    }
+    else if (val > BLUE_LIM)
+    {
+        return WATER;
+    }
+    else if (val > RED_LIM)
+    {
+        return FIRE;
+    }
+    else
+    {
+        return NONE;
+    }
+}
+
+/**
+ * @brief Calculates actual motor speed based on remaining battery power.
+ *
+ * @param percent desired motor percent
+ * @return actual motor percent
+ */
+float motorSpeed(float percent)
+{
+    return (11.5 / Battery.Voltage()) * percent;
+}
+
+/**
+ * @brief Opens/closes window.
+ *
+ * @param dir direction of opening/closing {FORWARDS = open, BACKWARDS = close}
+ */
+void moveWindow(Direction dir)
+{
+    float dist = 6.;
+    int counts = 2 * (UNIT_COUNTS * dist);
+
+    encoderR.ResetCounts();
+    encoderL.ResetCounts();
+
+    /*Actual power directed to motors to produce desired speed*/
+    float speed = motorSpeed(2 * F_POWER * dir);
+
+    float leftCorrection = 1.1;
+
+    rightMotor.SetPercent(speed);
+    leftMotor.SetPercent(speed * leftCorrection);
+
+    /*
+    Takes the average of the encoder counts and compares to (desired) counts
+
+    Division by 2 for average moved to other side of inequality and into
+    calculation of counts to save fractions of a second in computation time each loop
+    */
+    while (encoderR.Counts() + encoderL.Counts() < counts)
+        ;
+
+    stop();
+}
+
+/**
+ * @brief Whole process of opening and closing window.
+ */
+void openCloseWindow()
+{
+    /*Opens window*/
+    moveWindow(FORWARDS);
+    /*Moves to other side of window*/
+    windowReposition();
+    /*Closes window*/
+    moveWindow(BACKWARDS);
+}
+
+/**
+ * @brief Rotates the robot in place a specified amount.
+ *
+ * @param percent motor speed
+ * @param deg degrees for the bot to turn
+ * @param dir direction the robot turns
+ */
+void rotate(float percent, float deg, Direction dir)
+{
+    /*
+    Calculates circumference the wheels travel (BOT_WIDTH treated as diameter)
+    Multiples by fraction of the circle desired
+    Translates to counts
+
+    Explanation of multiplication by 2 is in while loop
+    */
+    float circumference = BOT_WIDTH * M_PI;
+    float circleFraction = deg / DEGREES;
+    int counts = 2 * (UNIT_COUNTS * (circumference * circleFraction));
+
+    encoderR.ResetCounts();
+    encoderL.ResetCounts();
+
+    /*Actual power directed to motors to produce desired speed*/
+    float speed = motorSpeed(percent * dir);
+
+    /*Starts each motor moving in different directions*/
+    rightMotor.SetPercent(RIGHT_MOTOR_CORRECTION * speed * -1);
+    leftMotor.SetPercent(LEFT_MOTOR_CORRECTION * speed);
+
+    /*
+    Takes the average of the encoder counts and compares to (desired) counts
+
+    Division by 2 for average moved to other side of inequality and into
+    calculation of counts to save fractions of a second in computation time each loop
+    */
+    while (encoderR.Counts() + encoderL.Counts() < counts)
+        ;
+
+    stop();
+}
+
+/**
  * @brief Identifies the state of the sensed line.
  *
  * @param prev previous state of the line
@@ -420,6 +543,77 @@ Line stateSense(Line prev)
     {
         return LINE_MIDDLE;
     }
+}
+
+/**
+ * @brief Stops the robot.
+ */
+void stop()
+{
+    rightMotor.Stop();
+    leftMotor.Stop();
+    /*Ensures robot comes to a complete stop*/
+    Sleep(0.25);
+}
+
+/**
+ * @brief Drives the robot straight ahead.
+ *
+ * @param percent speed the robot moves
+ */
+void straight(float percent)
+{
+    /*Moves each motor forward at same speed*/
+    rightMotor.SetPercent(percent * RIGHT_MOTOR_CORRECTION);
+    leftMotor.SetPercent(percent * LEFT_MOTOR_CORRECTION);
+}
+
+/**
+ * @brief Turns the robot a specified amount, breaking out if it encounters a pathing line.
+ *
+ * @param percent motor speed
+ * @param deg degrees for the bot to turn
+ * @param dir direction the robot turns (-1 for left, 1 for right)
+ */
+void turn(float percent, float deg, Direction dir)
+{
+    /*
+    Calculates circumference the wheels travel (BOT_WIDTH treated as radius)
+    Multiples by fraction of the circle desired
+    Translates to counts
+    */
+    float circumference = 2 * BOT_WIDTH * M_PI;
+    float circleFraction = deg / DEGREES;
+    int counts = (UNIT_COUNTS * (circumference * circleFraction));
+
+    encoderR.ResetCounts();
+    encoderL.ResetCounts();
+
+    /*Actual power directed to motors to produce desired speed*/
+    float speed = motorSpeed(percent);
+
+    /*Activates motors to turn the robot*/
+    switch (dir)
+    {
+    case LEFT:
+        rightMotor.SetPercent(speed);
+        break;
+    case RIGHT:
+        leftMotor.SetPercent(speed);
+        break;
+    default:
+        break;
+    }
+
+    /*
+    Sums encoder counts (one will be zero) and compares to counts
+
+    Breaks out if optosensors encounters a line
+    */
+    while (encoderR.Counts() + encoderL.Counts() < counts || (optol.Value() < L_DIV || optom.Value() < M_DIV || optor.Value() < R_DIV))
+        ;
+
+    stop();
 }
 
 /**
@@ -487,149 +681,15 @@ void turnOn(float percent, Direction dir)
 }
 
 /**
- * @brief Drives the robot straight ahead.
- *
- * @param percent speed the robot moves
+ * @brief Repositions robot on other side of window handle.
  */
-void straight(float percent)
+void windowReposition()
 {
-    /*Moves each motor forward at same speed*/
-    rightMotor.SetPercent(percent * RIGHT_MOTOR_CORRECTION);
-    leftMotor.SetPercent(percent * LEFT_MOTOR_CORRECTION);
-}
-
-/**
- * @brief Moves robot a specified amount.
- *
- * @param percent motor speed (if < 0 robot will drive backwards)
- * @param dist distance for the bot to travel (inches)
- */
-void forward(float percent, double dist)
-{
-    /*
-    Translates distance to counts
-
-    Explanation of multiplication by 2 is in while loop
-    */
-    int counts = 2 * (UNIT_COUNTS * dist);
-
-    encoderR.ResetCounts();
-    encoderL.ResetCounts();
-
-    /*Actual power directed to motors to produce desired speed*/
-    float speed = motorSpeed(percent);
-
-    straight(speed);
-
-    /*
-    Takes the average of the encoder counts and compares to (desired) counts
-
-    Division by 2 for average moved to other side of inequality and into
-    calculation of counts to save fractions of a second in computation time each loop
-    */
-    while (encoderR.Counts() + encoderL.Counts() < counts)
-        ;
-
-    stop();
-}
-
-/**
- * @brief Rotates the robot in place a specified amount.
- *
- * @param percent motor speed
- * @param deg degrees for the bot to turn
- * @param dir direction the robot turns
- */
-void rotate(float percent, float deg, Direction dir)
-{
-    /*
-    Calculates circumference the wheels travel (BOT_WIDTH treated as diameter)
-    Multiples by fraction of the circle desired
-    Translates to counts
-
-    Explanation of multiplication by 2 is in while loop
-    */
-    float circumference = BOT_WIDTH * PI;
-    float circleFraction = deg / DEGREES;
-    int counts = 2 * (UNIT_COUNTS * (circumference * circleFraction));
-
-    encoderR.ResetCounts();
-    encoderL.ResetCounts();
-
-    /*Actual power directed to motors to produce desired speed*/
-    float speed = motorSpeed(percent * dir);
-
-    /*Starts each motor moving in different directions*/
-    rightMotor.SetPercent(RIGHT_MOTOR_CORRECTION * speed * -1);
-    leftMotor.SetPercent(LEFT_MOTOR_CORRECTION * speed);
-
-    /*
-    Takes the average of the encoder counts and compares to (desired) counts
-
-    Division by 2 for average moved to other side of inequality and into
-    calculation of counts to save fractions of a second in computation time each loop
-    */
-    while (encoderR.Counts() + encoderL.Counts() < counts)
-        ;
-
-    stop();
-}
-
-/**
- * @brief Turns the robot a specified amount, breaking out if it encounters a pathing line.
- *
- * @param percent motor speed
- * @param deg degrees for the bot to turn
- * @param dir direction the robot turns (-1 for left, 1 for right)
- */
-void turn(float percent, float deg, Direction dir)
-{
-    /*
-    Calculates circumference the wheels travel (BOT_WIDTH treated as radius)
-    Multiples by fraction of the circle desired
-    Translates to counts
-    */
-    float circumference = 2 * BOT_WIDTH * PI;
-    float circleFraction = deg / DEGREES;
-    int counts = (UNIT_COUNTS * (circumference * circleFraction));
-
-    encoderR.ResetCounts();
-    encoderL.ResetCounts();
-
-    /*Actual power directed to motors to produce desired speed*/
-    float speed = motorSpeed(percent);
-
-    /*Activates motors to turn the robot*/
-    switch (dir)
-    {
-    case LEFT:
-        rightMotor.SetPercent(speed);
-        break;
-    case RIGHT:
-        leftMotor.SetPercent(speed);
-        break;
-    default:
-        break;
-    }
-
-    /*
-    Sums encoder counts (one will be zero) and compares to counts
-
-    Breaks out if optosensors encounters a line
-    */
-    while (encoderR.Counts() + encoderL.Counts() < counts || (optol.Value() < L_DIV || optom.Value() < M_DIV || optor.Value() < R_DIV))
-        ;
-
-    stop();
-}
-
-/**
- * @brief Stops the robot.
- */
-void stop()
-{
-    rightMotor.Stop();
-    leftMotor.Stop();
-    /*Ensures robot comes to a complete stop*/
-    Sleep(0.25);
+    drive(B_POWER, 1.);
+    rotate(F_POWER, 45., RIGHT);
+    drive(F_POWER, sqrt(2));
+    rotate(F_POWER, 90., LEFT);
+    drive(F_POWER, sqrt(2));
+    rotate(F_POWER, 45., RIGHT);
+    drive(B_POWER, 1.);
 }
